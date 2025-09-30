28493d1af15eb03e7a6a4883cf17d67a77a26c5b
aakankshakuhu <aakanksha200522@gmail.com>
2025-09-30 22:40:51 +0530

    find next permutation lexicographically

---
+++ Step 3/NextPermutation.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class NextPermutation {
+
+    public static void optimalapproach(int arr[], int n) {
+        int idx= -1;
+        for(int i = n-2; i >=0; i--) {
+            if(arr[i] < arr[i+1]) {
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) {
+            Arrays.sort(arr);
+            System.out.println(Arrays.toString(arr));
+            return;
+        }
+        for(int i = n-1; i >= 0; i--) {
+            if(arr[i] > arr[idx]) {
+                int temp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = temp;
+                break;
+            }
+        }
+        int left = idx+1, right = n-1;
+        while (left < right){
+            int temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+            left++;
+            right--;
+        }
+
+        
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 3, 2, 1, 0, 0};
+        int n = arr.length;
+        optimalapproach(arr, n);
+    }
+}
