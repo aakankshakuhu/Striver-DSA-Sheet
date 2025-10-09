c08645ba63474288b427dd9095da1e1a229b1f4f
aakankshakuhu <aakanksha200522@gmail.com>
2025-10-09 21:35:20 +0530

    .

--- Step 2/QuickSort.java
+++ Step 2/QuickSort.java
@@ -1,36 +1,50 @@
-import java.util.Arrays;
 public class QuickSort {
 
-    public static int partition(int arr[], int low, int high){
-        int pivot = arr[high];
-        int i = low - 1;
-        for(int j = low; j < high; j++){
-            if(arr[j] < pivot){
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; 
+        int i = low - 1;        
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
                 i++;
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-        i++;
-        int temp = arr[i];
-        arr[i] = temp;
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
         arr[high] = temp;
-        return i;
+
+        return i + 1;  
     }
 
-    public static void quickSort(int arr[], int low, int high){
-        if(low < high){
-            int pivotIdx = partition(int arr[], int low, int high);
-            quickSort(arr, low, pivotIdx-1);
-            quickSort(arr, pivotIdx+1, high);
-        }
-    }
-
-    public static void main(String args[]){
-        int[] arr = {29,10,14,37,14};
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
         int n = arr.length;
-        quickSort(arr, 0, n);
-        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
     }
 }
