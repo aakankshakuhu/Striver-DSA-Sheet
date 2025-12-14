e92bb0946380a7e2e2db5c242775894822bd3dd8
aakankshakuhu <aakanksha200522@gmail.com>
2025-12-14 20:16:13 +0530

    .

--- Step 3/CountSubarraySumEqualsK.java
+++ Step 3/CountSubarraySumEqualsK.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class CountSubarraySumEqualsK {
     
     public static int bruteforce(int arr[], int n, int given_sum) {
-        int len = 0, cnt = 0;
+        int cnt = 0;
         for(int i = 0; i < n; i++){
             long sum = 0;
             for(int j = i; j < n; j++) {
@@ -29,21 +29,18 @@ public class CountSubarraySumEqualsK {
             long rem = sum - k;
             
             if(preSum.containsKey(rem)){
-                int len = i - preSum.get(rem);
-                cnt++;
+                cnt += preSum.get(rem);
             }
 
-            if(!preSum.containsKey(sum)) { 
-                preSum.put(sum, i);
-            }
+            preSum.put(sum, preSum.getOrDefault(sum,0) + 1);
         }
         return cnt;
     }
 
     public static void main(String args[]){
-        int arr[] = {3, 1, 2, 4};
+        int arr[] = {9, -3, 3, -1, 6, -5};
         int n = arr.length;
-        int k = 6;
-        System.out.println("No. of subarrays with sum k: " + bruteforce(arr, n, k));
+        int k = 0;
+        System.out.println("No. of subarrays with sum k: " + hashmapapproach(arr, n, k));
     }
 }
