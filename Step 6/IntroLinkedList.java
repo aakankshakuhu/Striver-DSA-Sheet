6e3459267cb516f6b3008205a3c44ebf2530fd6b
aakankshakuhu <aakanksha200522@gmail.com>
2026-01-14 14:23:33 +0530

    creating a linked list, function to add node at first and last position, function to print a linked list

---
+++ Step 6/IntroLinkedList.java
@@ -0,0 +1,66 @@
+public class IntroLinkedList {
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    //add- first, last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+    }
+    
+    //print list
+    public void printList() {
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while(currNode != null){
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+
+        System.out.println("NULL");
+    }
+
+    public static void main(String args[]){
+        IntroLinkedList list = new IntroLinkedList();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("list");
+        list.addFirst("This");
+        list.printList();
+    }
+}
