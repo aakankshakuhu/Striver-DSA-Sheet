61f7bd2b2c002dfcf7d1b6014755c8af6c890eb2
aakankshakuhu <aakanksha200522@gmail.com>
2026-01-23 21:27:49 +0530

    created the linked list with a loop

---
+++ Step 6/LengthOfLoop.java
@@ -0,0 +1,41 @@
+class Node {
+    int data;
+    Node next;
+
+
+    Node(int data1) {
+        data = data1;
+        next = null;
+    }
+}
+
+
+public class LengthOfLoop {
+    public static void main(String[] args) {
+        // Creating a sample linked list with a loop
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        // Linking the nodes
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+
+        // Creating a loop from fifth to second
+        fifth.next = second;
+
+        Solution obj = new Solution();
+
+        int loopLength = obj.lengthOfLoop(head);
+
+        if (loopLength > 0) {
+            System.out.println("Length of the loop: " + loopLength);
+        } else {
+            System.out.println("No loop found in the linked list.");
+        }
+    }
+}
