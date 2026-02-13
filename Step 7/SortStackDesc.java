a42c56ab1cc5ff8386df73c5a32f3901af864a43
aakankshakuhu <aakanksha200522@gmail.com>
2026-02-13 12:00:19 +0530

    sorting stack with recursion

---
+++ Step 7/SortStackDesc.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+public class SortStackDesc {
+
+
+    public static void insert(Stack<Integer> stack, int temp){
+        if(stack.isEmpty() || stack.peek() <= temp){
+            stack.push(temp);
+            return;
+        }
+
+        int val = stack.pop();
+        insert(stack, temp);
+
+        stack.push(val);
+    }
+
+    public static void sortStack(Stack<Integer> stack){
+        if(!stack.isEmpty()){
+            int temp = stack.pop();
+            sortStack(stack);
+            insert(stack, temp);
+        }
+    }
+
+    
+
+    public static void main(String args[]){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(4);
+        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+
+        sortStack(stack);
+
+        System.out.print("Sorted Stack (descending order): ");
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
